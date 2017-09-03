30f0c72d64bc6af0712867d3ee028c1bccc08197
rodrigoaribeiro <digao.listas@gmail.com>
2017-09-03 19:07:18 -0300

    locale pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService} from './settings.service';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,19 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+/*    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    } */
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (SettingsService) => SettingsService.getLocale()
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- pipes/src/app/camel-case.pipe.ts
+++ pipes/src/app/camel-case.pipe.ts
@@ -6,12 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CamelCasePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    let values = value.split(' ')
+    let values = value.split(' ');
     let result = '';
     for (let v of values) {
-      result += v;
+      result += this.capitalize(v)+' ' ;
     }
     return result;
   }
-
+  capitalize(_value: string) {
+    return _value.substr(0, 1).toUpperCase() + _value.substr(1 ).toLowerCase();
+  }
 }
